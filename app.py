63e08ac6e39b2410765c848283d210151f3d3a21
Vijendarvjy <vijendareslavath19@gmail.com>
2026-04-24 23:09:00 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import plotly.express as px
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
